435f8017aabe289099142092fd4938544bdf3085
Yangtao Lin <yangtaolin938@gmail.com>
2026-04-29 18:56:23 -0400

    .

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-app.get("/hello", (req, res) => res.send("The server is updated!"));
 const mongoose = require("mongoose");
 const cors = require("cors");
 const indexRouter = require("./routes/index");
